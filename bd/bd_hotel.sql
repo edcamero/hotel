03b794771ebc3d059adec50235c1b70534f5361d
Alejandra Carreño <malejasone@gmail.com>
2019-07-18 21:28:45 -0500

    Update bd_hotel.sql

--- bd/bd_hotel.sql
+++ bd/bd_hotel.sql
@@ -1,213 +1,374 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-07-2019 a las 04:27:16
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.1.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE cliente (
-    id                integer NOT NULL,
-    persona_id        VARCHAR(15) NOT NULL,
-    tipo_cliente_id   integer NOT NULL,
-    usuario_id        integer NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE UNIQUE INDEX cliente__idx ON
-    cliente (
-        usuario_id
-    ASC );
+--
+-- Base de datos: `bd_hotel`
+--
 
-CREATE UNIQUE INDEX cliente__idxv1 ON
-    cliente (
-        persona_id
-    ASC );
+-- --------------------------------------------------------
 
-ALTER TABLE cliente ADD CONSTRAINT cliente_pk PRIMARY KEY ( id );
+--
+-- Estructura de tabla para la tabla `cliente`
+--
 
-CREATE TABLE empleado (
-    id                 integer NOT NULL,
-    persona_id         VARCHAR(15) NOT NULL,
-    tipo_empleado_id   integer NOT NULL,
-    usuario_id         integer NOT NULL
-);
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `persona_id` varchar(15) NOT NULL,
+  `tipo_cliente_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE UNIQUE INDEX empleado__idx ON
-    empleado (
-        persona_id
-    ASC );
+-- --------------------------------------------------------
 
-CREATE UNIQUE INDEX empleado__idxv1 ON
-    empleado (
-        usuario_id
-    ASC );
+--
+-- Estructura de tabla para la tabla `empleado`
+--
 
-ALTER TABLE empleado ADD CONSTRAINT empleado_pk PRIMARY KEY ( id );
+CREATE TABLE `empleado` (
+  `id` int(11) NOT NULL,
+  `persona_id` varchar(15) NOT NULL,
+  `tipo_empleado_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE hab_reserva (
-    habitacion_id         VARCHAR(3) NOT NULL,
-    reserva_id            integer NOT NULL,
-    reserva_id1           integer NOT NULL,
-    reserva_empleado_id   integer NOT NULL
-);
+-- --------------------------------------------------------
 
-ALTER TABLE hab_reserva
-    ADD CONSTRAINT hab_reserva_pk PRIMARY KEY ( habitacion_id,
-                                                reserva_id,
-                                                reserva_id1,
-                                                reserva_empleado_id );
+--
+-- Estructura de tabla para la tabla `habitacion`
+--
 
-CREATE TABLE habitacion (
-    id                   VARCHAR(3) NOT NULL,
-    tipo_habitacion_id   integer NOT NULL
-);
+CREATE TABLE `habitacion` (
+  `id` varchar(3) NOT NULL,
+  `tipo_habitacion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE habitacion ADD CONSTRAINT habitacion_pk PRIMARY KEY ( id );
+-- --------------------------------------------------------
 
-CREATE TABLE imagen (
-    id              integer NOT NULL,
-    nombre_imagen   VARCHAR(30) NOT NULL,
-    descripcion     VARCHAR(100),
-    ruta_img        VARCHAR(100) NOT NULL
-);
+--
+-- Estructura de tabla para la tabla `hab_reserva`
+--
 
-ALTER TABLE imagen ADD CONSTRAINT imagen_pk PRIMARY KEY ( id );
+CREATE TABLE `hab_reserva` (
+  `habitacion_id` varchar(3) NOT NULL,
+  `reserva_id` int(11) NOT NULL,
+  `reserva_id1` int(11) NOT NULL,
+  `reserva_empleado_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE persona (
-    id             VARCHAR(15) NOT NULL,
-    nombres        VARCHAR(30) NOT NULL,
-    apellidos      VARCHAR(30) NOT NULL,
-    direccion      VARCHAR(50) NOT NULL,
-    correo         VARCHAR(50) NOT NULL,
-    tipo_docu_id   integer(2) NOT NULL
-);
+-- --------------------------------------------------------
 
-CREATE UNIQUE INDEX persona__idx ON
-    persona (
-        tipo_docu_id
-    ASC );
+--
+-- Estructura de tabla para la tabla `imagen`
+--
 
-ALTER TABLE persona ADD CONSTRAINT persona_pk PRIMARY KEY ( id );
+CREATE TABLE `imagen` (
+  `id` int(11) NOT NULL,
+  `nombre_imagen` varchar(30) NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `ruta_img` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE reserva (
-    id              integer NOT NULL,
-    fecha_entrada   DATE NOT NULL,
-    fecha_salida    DATE NOT NULL,
-    total           integer,
-    descuento       FLOAT,
-    subtotal        integer,
-    cliente_id      integer NOT NULL,
-    empleado_id     integer NOT NULL
-);
+-- --------------------------------------------------------
 
-ALTER TABLE reserva
-    ADD CONSTRAINT reserva_pk PRIMARY KEY ( id,
-                                            cliente_id,
-                                            empleado_id );
+--
+-- Estructura de tabla para la tabla `persona`
+--
 
-CREATE TABLE telefonos (
-    id           integer NOT NULL,
-    telefono     VARCHAR(10) NOT NULL,
-    persona_id   VARCHAR(15) NOT NULL
-);
+CREATE TABLE `persona` (
+  `id` varchar(15) NOT NULL,
+  `nombres` varchar(30) NOT NULL,
+  `apellidos` varchar(30) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `tipo_docu_id` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE telefonos ADD CONSTRAINT telefonos_pk PRIMARY KEY ( id );
+--
+-- Volcado de datos para la tabla `persona`
+--
 
-CREATE TABLE tipo_cliente (
-    id          integer NOT NULL,
-    nombre      VARCHAR(30) NOT NULL,
-    descuento   FLOAT
-);
+INSERT INTO `persona` (`id`, `nombres`, `apellidos`, `direccion`, `correo`, `tipo_docu_id`) VALUES
+('1514142', 'ahhaha', 'hahahsh', 'hshshshsq', 'm-a-c@hotmail.com', 1),
+('15141422', 'ahhaha', 'hahahsh', 'hshshshsq', 'm2-a-c@hotmail.com', 2),
+('15143344', 'ahhaha', 'hahahsh', 'hshshshsq', 'c@hotmail.com', 3);
 
-ALTER TABLE tipo_cliente ADD CONSTRAINT tipo_cliente_pk PRIMARY KEY ( id );
+-- --------------------------------------------------------
 
-CREATE TABLE tipo_docu (
-    id       integer(2) NOT NULL,
-    nombre   VARCHAR(15) NOT NULL
-);
+--
+-- Estructura de tabla para la tabla `reserva`
+--
 
-ALTER TABLE tipo_docu ADD CONSTRAINT tipo_docu_pk PRIMARY KEY ( id );
+CREATE TABLE `reserva` (
+  `id` int(11) NOT NULL,
+  `fecha_entrada` date NOT NULL,
+  `fecha_salida` date NOT NULL,
+  `total` int(11) DEFAULT NULL,
+  `descuento` float DEFAULT NULL,
+  `subtotal` int(11) DEFAULT NULL,
+  `cliente_id` int(11) NOT NULL,
+  `empleado_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE tipo_empleado (
-    id       integer NOT NULL,
-    nombre   VARCHAR(40)
-);
+-- --------------------------------------------------------
 
-ALTER TABLE tipo_empleado ADD CONSTRAINT tipo_empleado_pk PRIMARY KEY ( id );
+--
+-- Estructura de tabla para la tabla `telefonos`
+--
 
-CREATE TABLE tipo_habitacion (
-    id            integer NOT NULL,
-    nombre_tipo   VARCHAR(30) NOT NULL,
-    precio        integer NOT NULL,
-    imagen_id     integer NOT NULL
-);
+CREATE TABLE `telefonos` (
+  `id` int(11) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `persona_id` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE UNIQUE INDEX tipo_habitacion__idx ON
-    tipo_habitacion (
-        imagen_id
-    ASC );
+-- --------------------------------------------------------
 
-ALTER TABLE tipo_habitacion ADD CONSTRAINT tipo_habitacion_pk PRIMARY KEY ( id );
+--
+-- Estructura de tabla para la tabla `tipo_cliente`
+--
 
-CREATE TABLE usuario (
-    id            integer NOT NULL,
-    nombre_user   VARCHAR(30) NOT NULL,
-    password      VARCHAR(32) NOT NULL,
-    rol           integer
-);
+CREATE TABLE `tipo_cliente` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `descuento` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE usuario ADD CONSTRAINT usuario_pk PRIMARY KEY ( id );
+--
+-- Volcado de datos para la tabla `tipo_cliente`
+--
 
-ALTER TABLE cliente
-    ADD CONSTRAINT cliente_persona_fk FOREIGN KEY ( persona_id )
-        REFERENCES persona ( id );
+INSERT INTO `tipo_cliente` (`id`, `nombre`, `descuento`) VALUES
+(1, 'habitual', 0.25),
+(2, 'esporadico', 0);
 
-ALTER TABLE cliente
-    ADD CONSTRAINT cliente_tipo_cliente_fk FOREIGN KEY ( tipo_cliente_id )
-        REFERENCES tipo_cliente ( id );
+-- --------------------------------------------------------
 
-ALTER TABLE cliente
-    ADD CONSTRAINT cliente_usuario_fk FOREIGN KEY ( usuario_id )
-        REFERENCES usuario ( id );
+--
+-- Estructura de tabla para la tabla `tipo_docu`
+--
 
-ALTER TABLE empleado
-    ADD CONSTRAINT empleado_persona_fk FOREIGN KEY ( persona_id )
-        REFERENCES persona ( id );
+CREATE TABLE `tipo_docu` (
+  `id` int(2) NOT NULL,
+  `nombre` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE empleado
-    ADD CONSTRAINT empleado_tipo_empleado_fk FOREIGN KEY ( tipo_empleado_id )
-        REFERENCES tipo_empleado ( id );
+--
+-- Volcado de datos para la tabla `tipo_docu`
+--
 
-ALTER TABLE empleado
-    ADD CONSTRAINT empleado_usuario_fk FOREIGN KEY ( usuario_id )
-        REFERENCES usuario ( id );
+INSERT INTO `tipo_docu` (`id`, `nombre`) VALUES
+(1, 'cedula'),
+(2, 'tarjeta identid'),
+(3, 'pasaporte');
 
-ALTER TABLE hab_reserva
-    ADD CONSTRAINT hab_reserva_habitacion_fk FOREIGN KEY ( habitacion_id )
-        REFERENCES habitacion ( id );
+-- --------------------------------------------------------
 
-ALTER TABLE hab_reserva
-    ADD CONSTRAINT hab_reserva_reserva_fk FOREIGN KEY ( reserva_id,
-                                                        reserva_id1,
-                                                        reserva_empleado_id )
-        REFERENCES reserva ( id,
-                             cliente_id,
-                             empleado_id );
+--
+-- Estructura de tabla para la tabla `tipo_empleado`
+--
 
-ALTER TABLE habitacion
-    ADD CONSTRAINT habitacion_tipo_habitacion_fk FOREIGN KEY ( tipo_habitacion_id )
-        REFERENCES tipo_habitacion ( id );
+CREATE TABLE `tipo_empleado` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE persona
-    ADD CONSTRAINT persona_tipo_docu_fk FOREIGN KEY ( tipo_docu_id )
-        REFERENCES tipo_docu ( id );
+-- --------------------------------------------------------
 
-ALTER TABLE reserva
-    ADD CONSTRAINT reserva_cliente_fk FOREIGN KEY ( cliente_id )
-        REFERENCES cliente ( id );
+--
+-- Estructura de tabla para la tabla `tipo_habitacion`
+--
 
-ALTER TABLE reserva
-    ADD CONSTRAINT reserva_empleado_fk FOREIGN KEY ( empleado_id )
-        REFERENCES empleado ( id );
+CREATE TABLE `tipo_habitacion` (
+  `id` int(11) NOT NULL,
+  `nombre_tipo` varchar(30) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `imagen_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE telefonos
-    ADD CONSTRAINT telefonos_persona_fk FOREIGN KEY ( persona_id )
-        REFERENCES persona ( id );
+-- --------------------------------------------------------
 
-ALTER TABLE tipo_habitacion
-    ADD CONSTRAINT tipo_habitacion_imagen_fk FOREIGN KEY ( imagen_id )
-        REFERENCES imagen ( id );
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nombre_user` varchar(30) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `rol` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cliente__idx` (`usuario_id`),
+  ADD UNIQUE KEY `cliente__idxv1` (`persona_id`),
+  ADD KEY `cliente_tipo_cliente_fk` (`tipo_cliente_id`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `empleado__idx` (`persona_id`),
+  ADD UNIQUE KEY `empleado__idxv1` (`usuario_id`),
+  ADD KEY `empleado_tipo_empleado_fk` (`tipo_empleado_id`);
+
+--
+-- Indices de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `habitacion_tipo_habitacion_fk` (`tipo_habitacion_id`);
+
+--
+-- Indices de la tabla `hab_reserva`
+--
+ALTER TABLE `hab_reserva`
+  ADD PRIMARY KEY (`habitacion_id`,`reserva_id`,`reserva_id1`,`reserva_empleado_id`),
+  ADD KEY `hab_reserva_reserva_fk` (`reserva_id`,`reserva_id1`,`reserva_empleado_id`);
+
+--
+-- Indices de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `persona__idx` (`tipo_docu_id`) USING BTREE;
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id`,`cliente_id`,`empleado_id`),
+  ADD KEY `reserva_cliente_fk` (`cliente_id`),
+  ADD KEY `reserva_empleado_fk` (`empleado_id`);
+
+--
+-- Indices de la tabla `telefonos`
+--
+ALTER TABLE `telefonos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `telefonos_persona_fk` (`persona_id`);
+
+--
+-- Indices de la tabla `tipo_cliente`
+--
+ALTER TABLE `tipo_cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo_docu`
+--
+ALTER TABLE `tipo_docu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo_empleado`
+--
+ALTER TABLE `tipo_empleado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo_habitacion`
+--
+ALTER TABLE `tipo_habitacion`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tipo_habitacion__idx` (`imagen_id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_persona_fk` FOREIGN KEY (`persona_id`) REFERENCES `persona` (`id`),
+  ADD CONSTRAINT `cliente_tipo_cliente_fk` FOREIGN KEY (`tipo_cliente_id`) REFERENCES `tipo_cliente` (`id`),
+  ADD CONSTRAINT `cliente_usuario_fk` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_persona_fk` FOREIGN KEY (`persona_id`) REFERENCES `persona` (`id`),
+  ADD CONSTRAINT `empleado_tipo_empleado_fk` FOREIGN KEY (`tipo_empleado_id`) REFERENCES `tipo_empleado` (`id`),
+  ADD CONSTRAINT `empleado_usuario_fk` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
+
+--
+-- Filtros para la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD CONSTRAINT `habitacion_tipo_habitacion_fk` FOREIGN KEY (`tipo_habitacion_id`) REFERENCES `tipo_habitacion` (`id`);
+
+--
+-- Filtros para la tabla `hab_reserva`
+--
+ALTER TABLE `hab_reserva`
+  ADD CONSTRAINT `hab_reserva_habitacion_fk` FOREIGN KEY (`habitacion_id`) REFERENCES `habitacion` (`id`),
+  ADD CONSTRAINT `hab_reserva_reserva_fk` FOREIGN KEY (`reserva_id`,`reserva_id1`,`reserva_empleado_id`) REFERENCES `reserva` (`id`, `cliente_id`, `empleado_id`);
+
+--
+-- Filtros para la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD CONSTRAINT `persona_tipo_docu_fk` FOREIGN KEY (`tipo_docu_id`) REFERENCES `tipo_docu` (`id`);
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `reserva_cliente_fk` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `reserva_empleado_fk` FOREIGN KEY (`empleado_id`) REFERENCES `empleado` (`id`);
+
+--
+-- Filtros para la tabla `telefonos`
+--
+ALTER TABLE `telefonos`
+  ADD CONSTRAINT `telefonos_persona_fk` FOREIGN KEY (`persona_id`) REFERENCES `persona` (`id`);
+
+--
+-- Filtros para la tabla `tipo_habitacion`
+--
+ALTER TABLE `tipo_habitacion`
+  ADD CONSTRAINT `tipo_habitacion_imagen_fk` FOREIGN KEY (`imagen_id`) REFERENCES `imagen` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
